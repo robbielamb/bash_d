fff249c51dd791bdee5162244a7c8e470b51e0a7
Robbie Lamb <robbie.lamb@gmail.com>
2010-06-14 13:28:22 -0600

    Removing path_helper command

--- all/900_osx_env_sync.sh
+++ all/900_osx_env_sync.sh
@@ -1,9 +1,4 @@
-if [ -x /usr/libexec/path_helper ]; then
-
-eval `/usr/libexec/path_helper -s`
-
-  if [[ -s $HOME/.MacOSX/environment ]] ; then
-    defaults write $HOME/.MacOSX/environment PATH "$PATH"
-  fi
-
+if [[ -s $HOME/.MacOSX/environment ]] ; then
+  defaults write $HOME/.MacOSX/environment PATH "$PATH"
 fi
+
